8054083fca07d8b70152a053b7e603ca0a0c3040
Nandinbayr9809 <bnandinbayr53@yahoo.com>
2017-04-25 11:21:31 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ namespace Biedaalt
                 }
 
             }
-            speed = 100;
+            speed = 300;
             for (int i = 0; i < map.GetLength(0); i++)
             {
                 for (int j = 0; j < map.GetLength(1); j++)
